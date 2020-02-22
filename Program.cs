f8e02e85ee038f3a0d073b4c82c3cb637cd84f4d
Churrro696 <david.nunez193@tectijuana.edu.mx>
2020-02-22 00:25:26 -0800

    Peliculas(publico)

--- Program.cs
+++ Program.cs
@@ -1,27 +1,12 @@
 ﻿using System;
 public class Pelicula
 {
- private string titulo;
- private int año;
- private string pais;
- private string director;
+    public string titulo;
+	public int año;
+	public string pais;
+	public string director;
+	
 
- public void SetTitulo(string n){
-    titulo = n;
- }
- public string GetTitulo()
- {
-    return titulo;
- }
- public void SetAño(int y)
- {
-    
-    año = y;
- }
- public int GetAño()
- {
-    return año;
- }
 
 
 }
@@ -32,16 +17,18 @@ namespace POO_act_2
 {
      class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Pelicula p1 = new Pelicula ();
-            p1.SetTitulo("Wall-E");
-            p1.SetAño(2008);
-
-            Pelicula p2 = new Pelicula();
-            p2.SetTitulo("Ratatouille");
-            p2.SetAño(2007);
-            Console.WriteLine(p1.GetTitulo() + "\t" + p1.GetAño() + "\n" + p2.GetTitulo() + "\t" + p2.GetAño());
+            Pelicula p1 = new Pelicula();
+		p1.titulo = "Wall-E";
+		p1.año = 2008;
+		
+		Pelicula p2 = new Pelicula();
+		p2.titulo = "Ratatouille";
+		p2.año = 2016;
+		
+		Console.WriteLine(p1.titulo + "\t" + p1.año + "\n" + p2.titulo + "\t" + p2.año);
+		
 
 
         }
